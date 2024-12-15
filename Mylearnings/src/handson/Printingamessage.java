ef9668e2b787facee34d9d541a057fa92b99fd68
user <user@DESKTOP-L9CAQAB>
2024-12-15 09:10:55 +0000

    Uploadcode

---
+++ Mylearnings/src/handson/Printingamessage.java
@@ -0,0 +1,14 @@
+package handson;
+
+public class Printingamessage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println(" Welcome to the Automation Learnings");
+		
+		
+	}
+
+}
